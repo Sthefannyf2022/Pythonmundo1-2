7ff7de8819422e958805f324f301bf9adcd6b4ce
Sthefanny Farias <110677502+Sthefannyf2022@users.noreply.github.com>
2022-11-05 18:50:18 -0300

    Add files via upload

---
+++ ex15.py
@@ -0,0 +1,7 @@
+''' Exercicio 15 python mundo 1 - Curso em video'''
+
+dias = float(input('Quantos os dias ficou com o carro?'))
+km = float(input('Quantos km percorreu ?'))
+valor_total = (dias * 60) + (km * 0.15)
+
+print('O valor total é {} R$'.format(valor_total))
